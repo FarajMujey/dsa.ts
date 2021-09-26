e4d248289490ec9c83f1ed7f5d371845f593bdfc
FarajMujey!! <87531939+FarajMujey@users.noreply.github.com>
2021-09-26 11:30:34 +0800

    Delete src/test directory

--- src/test/ds/hash-table.test.ts
+++
@@ -1,9 +0,0 @@
-import HashTable from '../../ds/hash-table'
-
-const hashTable = new HashTable<string, number>()
-
-hashTable.set('foo', 200)
-hashTable.set('bar', 400)
-hashTable.set('baz', 18)
-
-console.log(hashTable + '')
